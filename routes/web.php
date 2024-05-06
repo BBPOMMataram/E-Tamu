cf831c2372a08641ddb63b24f9186bf5030f07a8
arfani <arfanihidayat@gmail.com>
2024-05-06 15:31:42 +0800

    update route to 'security'

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GuestController;
 use App\Http\Controllers\PresensiController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Guest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PresensiController::class, 'form'])->name('presensi.form');
@@ -14,7 +15,7 @@ Route::middleware(['auth', 'verified'])->prefix('security')->group(function(){
     })->name('dashboard');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('security')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

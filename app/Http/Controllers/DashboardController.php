7b91c22f3faef5e8b4528f054b3016a4ce6bb08e
arfani <arfanihidayat@gmail.com>
2024-09-10 09:30:43 +0800

    fix data 0 jika tidak ada filter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,10 +13,18 @@ class DashboardController extends Controller
         $year = $request->query('year');
         $month = $request->query('month');
 
-        $guests = Guest::with(['services', 'serviceLainnya'])
-        ->whereYear('created_at', $year)
-        ->whereMonth('created_at', $month)
-        ->get();
+        $guests = Guest::with(['services', 'serviceLainnya']);
+         
+        if($year){
+            $guests->whereYear('created_at', $year);
+        }
+
+        if($month){
+            $guests->whereMonth('created_at', $month);
+        }
+
+        $guests = $guests->get();
+
         $services = Service::all();
 
         $guests_today = Guest::whereDate('created_at', Carbon::today())->count();

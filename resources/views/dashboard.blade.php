42ea5d38af667bb8f59f317347d0d93a8ca83646
arfani <arfanihidayat@gmail.com>
2024-09-10 09:49:01 +0800

    update text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -54,9 +54,9 @@
             </div>
 
             
-            <div class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-4 text-center mb-2">
-                <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100">
-                    Total Seluruh Tamu Berdasarkan Keperluan
+            <div class="bg-gray-800 text-white shadow-md rounded-lg p-4 text-center mb-2">
+                <h2 class="text-xl font-semibold">
+                    Total Tamu Berdasarkan Keperluan
                 </h2>
             </div>
 
